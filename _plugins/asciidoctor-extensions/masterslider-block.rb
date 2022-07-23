494eea648c2722ab457e302458c2d43b1d3fd9d9
Juergen Adams <juergen@jekyll.one>
2022-07-23 14:33:20 +0200

    New version 2022.4.10

---
+++ _plugins/asciidoctor-extensions/masterslider-block.rb
@@ -0,0 +1,47 @@
+# ------------------------------------------------------------------------------
+# ~/_plugins/asciidoctor-extensions/carousel-block.rb
+# Asciidoctor extension for J1 Carousel (Owl Carousel)
+#
+# Product/Info:
+# https://jekyll.one
+#
+# Copyright (C) 2022 Juergen Adams
+#
+# J1 Template is licensed under the MIT License.
+# See: https://github.com/jekyll-one-org/J1 Template/blob/master/LICENSE
+#
+# ------------------------------------------------------------------------------
+require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
+include Asciidoctor
+
+# A block macro that embeds a (Master) Slider (patent) block
+# into the output document
+#
+# Usage:
+#
+#   masterslider::slider_id[role="additional classes"]
+#
+# Example:
+#
+#   .The slider title
+#   masterslider::ms_00001[role="mt-3 mb-5"]
+#
+Asciidoctor::Extensions.register do
+
+  class MasterSliderBlockMacro < Extensions::BlockMacroProcessor
+    use_dsl
+
+    named :masterslider
+    name_positional_attributes 'role'
+
+    def process parent, target, attrs
+
+      title_html  = (attrs.has_key? 'title') ? %(<div class="slider-title">#{attrs['title']}</div>\n) : nil
+      html = %(#{title_html} <div id="p_#{target}" class="master-slider-parent #{attrs['role']}"></div>)
+      create_pass_block parent, html, attrs, subs: nil
+    end
+  end
+
+  block_macro MasterSliderBlockMacro
+
+end
--- _plugins/lunr_index.rb
+++ _plugins/lunr_index.rb
@@ -369,6 +369,6 @@ end
 
 module Jekyll
   module J1LunrSearch
-    VERSION = '2022.4.7'
+    VERSION = '2022.4.10'
   end
 end
